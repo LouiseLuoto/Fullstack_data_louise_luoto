0bb112f3b5eb7865d9908349de4e9b150f705e1e
Louise <louiseluoto@hotmail.com>
2024-09-18 14:50:07 +0200

    Changed file structure

--- exercises/3_chatbot/dashboard.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from components.bot import Bot
-
-def initialize_session_state():
-    """Initialize session state variables"""
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    if "bot" not in st.session_state:
-        st.session_state.bot = Bot()
